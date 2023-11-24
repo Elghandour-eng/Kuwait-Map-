3c6a4af49da4823d3aad7fca8f1f8abffc201057
Abdelwahab Elghnadour <a.elghandour@cequens.com>
2023-11-24 14:53:30 +0200

    edit features type

--- mapConfig.js
+++ mapConfig.js
@@ -2,26 +2,27 @@ import { checkDevice } from './responsive_phone.js';
 
 
 export var mapProperties = {
-    center: { lat: 29.3759, lng: 47.9774 }, // Centered at Kuwait
-    zoom: (checkDevice() === "Mobile Device") ? 10 : 7.5,
-    mapId: (checkDevice() === "Mobile Device") ? "13c8646847f68c72" : "6f5b7f86f2745f19",
-    disableDefaultUI: true,
-    minZoom: 8 - 2,
-    restriction: {
-        latLngBounds: {
-            north: 30.095, // Kuwait north
-            south: 28.524, // Kuwait south
-            west: 46.372, // Kuwait west
-            east: 48.421 // Kuwait east
-        },
-    },
-    styles:[ 
-        {
-          featureType:"all",
-          stylers:[
-            {saturation:-80}
-          ]
-        }
-      ]  
-    
+  center: { lat: 29.3759, lng: 47.9774 }, // Centered at Kuwait
+  zoom: (checkDevice() === "Mobile Device") ? 10 : 7.5,
+  mapId: (checkDevice() === "Mobile Device") ? "13c8646847f68c72" : "6f5b7f86f2745f19",
+  disableDefaultUI: true,
+  minZoom: 8 - 2,
+  maxZoom: 14,
+  restriction: {
+      latLngBounds: {
+          north: 30.095, // Kuwait north
+          south: 28.524, // Kuwait south
+          west: 46.372, // Kuwait west
+          east: 48.421 // Kuwait east
+      },
+  },
+  styles:[ 
+      {
+        featureType:"all",
+        elementType:"all",
+        stylers:[
+          {visibility:"off"}
+        ]
+      }
+    ]  
 };
